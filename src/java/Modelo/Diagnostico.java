4ccc68aea5dc3c100c1b523e654f2887a12a8956
vlady2 <vladimirvides1@gmail.com>
2024-11-22 10:26:52 -0600

    insercion de entidad de diagnostico

--- src/java/Modelo/Diagnostico.java
+++ src/java/Modelo/Diagnostico.java
@@ -14,4 +14,56 @@ public class Diagnostico {
     int FkEnfermedad;
     String descripcion;
     String tratamiento;
+
+    public Diagnostico() {
+    }
+    
+    public Diagnostico(int IdDiagnostico, int FkCita, int FkEnfermedad, String descripcion, String tratamiento) {
+        this.IdDiagnostico = IdDiagnostico;
+        this.FkCita = FkCita;
+        this.FkEnfermedad = FkEnfermedad;
+        this.descripcion = descripcion;
+        this.tratamiento = tratamiento;
+    }
+    
+    public int getIdDiagnostico() {
+        return IdDiagnostico;
+    }
+
+    public void setIdDiagnostico(int IdDiagnostico) {
+        this.IdDiagnostico = IdDiagnostico;
+    }
+
+    public int getFkCita() {
+        return FkCita;
+    }
+
+    public void setFkCita(int FkCita) {
+        this.FkCita = FkCita;
+    }
+
+    public int getFkEnfermedad() {
+        return FkEnfermedad;
+    }
+
+    public void setFkEnfermedad(int FkEnfermedad) {
+        this.FkEnfermedad = FkEnfermedad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+    
 }
